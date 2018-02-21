9c88a97ef48e84bfe6af9d83a0d0742821efb627
Willian Hayashida <will.hayashida07@gmail.com>
2018-02-21 00:35:52 -0300

    message

--- scripts/run_crs_DE-local.sh
+++ scripts/run_crs_DE-local.sh
@@ -3,12 +3,13 @@
 echo "Running CRS with DE and openMP locally"
 #lscpu
 
-CEPETRO_PATH="../../cepetro/cepetro-codes"
+CEPETRO="../../cepetro"
+CEPETRO_CODES="../../cepetro/cepetro-codes"
 sudo apt update
 sudo apt install g++
 
-#ls $CEPETRO_PATH
+#ls $CEPETRO_CODES
 
-g++ -std=c++11 $CEPETRO_PATH/src/main-openMP.cpp $CEPETRO_PATH/src/File.cpp $CEPETRO_PATH/src/SuFile.cpp $CEPETRO_PATH/src/DifferentialEvolution.cpp -o main-openMP -fopenmp -g -O3
+g++ -std=c++11 $CEPETRO_CODES/src/main-openMP.cpp $CEPETRO_CODES/src/File.cpp $CEPETRO_CODES/src/SuFile.cpp $CEPETRO_CODES/src/DifferentialEvolution.cpp -o main-openMP -fopenmp -g -O3
 
-time ./main-openMP ../data/simple-syntetic-micro_sorted.su 1000 0.0 100 0.00015 70 10 10
+time $CEPETRO_CODES/main-openMP $CEPETRO/data/simple-syntetic-micro_sorted.su 1000 0.0 100 0.00015 70 10 10
