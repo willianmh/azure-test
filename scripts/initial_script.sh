36bfa8eb88f4a2d05f3263b600c4866fb22aa6a6
Willian Hayashida <will.hayashida07@gmail.com>
2018-02-22 00:11:56 -0300

    m

--- scripts/initial_script.sh
+++ scripts/initial_script.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+LOGFILE="/home/username/instalation.log"
+
+#check network conction
+ATTEMPTS=0
+while [ $(nc -zw1 google.com 443) ] && [ "$ATTEMPTS" -lt 5 ]; do
+  echo "we have NO connectivity" &>> ${LOGFILE}
+  sleep 15
+  ATTEMPTS=$((ATTEMPTS+1))
+done
+
 #Install MPI and dependencies
+export DEBIAN_FRONTEND=noninteractive
+
+touch hey.txt
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install g++ gcc htop
+sudo apt-get -y install g++
 
 sudo mkdir /home/username/mymountpoint
 echo "${1}" > pass
@@ -12,9 +26,7 @@ rm pass
 sudo mount -a
 
 # sudo mount -t cifs //machinetesti.file.core.windows.net/doc /home/username/mymountpoint -o vers=3.0,username=machinetesti,password=pDyMoTChRGvm1/Pp0sz4USreLsttxoDa2xLKp/JXYWzppPUquesDLD7jerlLqdxSYGOGsLqRe8uYTLwtBW+AhQ==,dir_mode=0777,file_mode=0777,sec=ntlmssp
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install g++ gcc htop
+
 sudo touch /home/username/mymountpoint/ola.txt
 
 echo "config done"
